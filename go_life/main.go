bb4ceebac607d915575e1b99a0fb00b8a4ffa8a2
wangshaobo <fireshooter@163.com>
2020-01-04 10:36:12 +0800

    go func life circle with main

---
+++ go_life/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+import "time"
+
+func main() {
+	fmt.Println("main start")
+	defer fmt.Println("main quit")
+
+	go func() {
+		fmt.Println("go func start")
+		defer fmt.Println("go func quit")
+
+		for count := 0; ; count++ {
+			fmt.Printf("count: %v\n", count)
+			time.Sleep(500 * time.Millisecond)
+			if count > 10 {
+				break
+			}
+		}
+	}()
+
+	// try sleep 0 / 2 / 5 second to check life circle
+	time.Sleep(2 * time.Second)
+}
